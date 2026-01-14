f032b01c686122fe9a88554e1df8d3f536be8a41
김대훈 <eogns136@gmail.com>
2026-01-14 12:37:40 +0900

    Feat: Validation으로 signupRequest 검증

--- src/main/java/e3i2/ecommerce_backoffice/domain/admin/controller/AdminController.java
+++ src/main/java/e3i2/ecommerce_backoffice/domain/admin/controller/AdminController.java
@@ -3,6 +3,7 @@ package e3i2.ecommerce_backoffice.domain.admin.controller;
 import e3i2.ecommerce_backoffice.domain.admin.dto.SignupRequest;
 import e3i2.ecommerce_backoffice.domain.admin.dto.SignupResponse;
 import e3i2.ecommerce_backoffice.domain.admin.service.AdminService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class AdminController {
     //관리자 회원가입
     @PostMapping("/api/admins/signup")
     public ResponseEntity<SignupResponse> signup(
-            @RequestBody SignupRequest request
+            @Valid @RequestBody SignupRequest request
     ) {
         return ResponseEntity.status(HttpStatus.CREATED).body(adminService.signup(request));
     }
--- src/main/java/e3i2/ecommerce_backoffice/domain/admin/service/AdminService.java
+++ src/main/java/e3i2/ecommerce_backoffice/domain/admin/service/AdminService.java
@@ -4,10 +4,8 @@ import e3i2.ecommerce_backoffice.common.config.PasswordEncoder;
 import e3i2.ecommerce_backoffice.domain.admin.dto.SignupRequest;
 import e3i2.ecommerce_backoffice.domain.admin.dto.SignupResponse;
 import e3i2.ecommerce_backoffice.domain.admin.entity.Admin;
-import e3i2.ecommerce_backoffice.domain.admin.entity.AdminStatus;
 import e3i2.ecommerce_backoffice.domain.admin.repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
